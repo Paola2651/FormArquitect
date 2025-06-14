403385a452b560353c52cbfed7a5d707083aaff4
Paola Echeverria <p.andrea85@hotmail.com>
2025-06-14 10:19:31 -0500

    Cargue Formulario

---
+++ Formulario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello word<h1>
+    <hr>
+    <br>
+    <h2>Hello word<h2>
+    <h3>Hello word<h3>
+
+    <form action="" method="post">
+        
+    </form>
+</body>
+</html>
